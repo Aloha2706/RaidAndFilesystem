ecdbf3e0dbaa489c87c78de369bdd32e9a080d4d
Aloha2706 <99888571+Aloha2706@users.noreply.github.com>
2022-11-25 08:04:24 +0300

    Update CreateRaid5.sh

--- CreateRaid5.sh
+++ CreateRaid5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
-sudo mdadm --create --verbose /dev/md0 --level 5 --raid-devices 5 /dev/sd{b,c,d,e,f}
+sudo mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f}
